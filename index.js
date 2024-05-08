59f92c22d3201b2d7af68398e391ddb0b5e96004
Ayesha_Frontend_Web <ayesha@724.one>
2024-05-08 15:59:41 +0500

    practice

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Javascript
+// let obj={
+//     name:"sachin",
+// }
+// obj.age=40;
+
+// document.getElementById("demo").innerHTML=JSON.stringify(obj);
+
+// console.log(obj)
+
+// Object.freeze(obj)
+// obj.field="reactjs";
+// console.log(obj)
+
